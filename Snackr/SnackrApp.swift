dc2e428134cf89a465519abbb0c6d98b209d8ccd
Tyler Maxwell <111320575+maxwellt23@users.noreply.github.com>
2025-04-07 13:37:54 -0600

    Initial Commit

---
+++ Snackr/SnackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackrApp.swift
+//  Snackr
+//
+//  Created by Tyler Maxwell on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SnackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
